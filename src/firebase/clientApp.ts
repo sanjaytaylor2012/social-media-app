382ea9572621ce1e1fbd147a0deab81889a6413b
sanjaytaylor2012 <sanjaytaylor2012@gmail.com>
2023-04-05 11:50:42 -0400

    changing env settings

--- src/firebase/clientApp.ts
+++ src/firebase/clientApp.ts
@@ -3,24 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"; //used to store json key value pairs
 import { getStorage } from "firebase/storage"; //used to store media
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-// };
-
 const firebaseConfig = {
-  apiKey: "AIzaSyBjwjRjVElMZ0yhLG_X0Hz3y_bbH3_iDvw",
-  authDomain: "instagram-clone-40909.firebaseapp.com",
-  projectId: "instagram-clone-40909",
-  storageBucket: "instagram-clone-40909.appspot.com",
-  messagingSenderId: "1064462505615",
-  appId: "1:1064462505615:web:6aafa78a1160de980d42fa",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
